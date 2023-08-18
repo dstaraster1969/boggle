a222eb7d79504fb3a30539c02db817fd4725441e
dstaraster1969 <71042914+dstaraster1969@users.noreply.github.com>
2023-08-18 09:27:03 -0700

    started on game logic

--- board.py
+++ board.py
@@ -1,14 +1,18 @@
+from PyDictionary import PyDictionary
+
 from dice import Dice
 
 
 class Board:
     def __init__(self):
+        self.dictionary = PyDictionary()
         self.board = [
             [0 for i in range(4)],
             [0 for i in range(4)],
             [0 for i in range(4)],
             [0 for i in range(4)],
         ]
+        self.words = []
 
     def setup_board(self):
         dice_counter = 0
@@ -17,5 +21,39 @@ class Board:
                 self.board[i][j] = Dice().roll_die(dice_counter)
                 dice_counter += 1
 
-        print(self.board)
+    def generate_list_of_words(self, word, i, j, coordinates_visited):
+        word, finished = self.build_word(word, i, j)
+        coordinates_visited.append([i, j])
+        if len(word) >=3 and self.check_dictionary_for_word(word):
+            self.words.append(word)
+        if not finished:
+            if not [i-1, j-1] in coordinates_visited:
+                self.generate_list_of_words(word, i-1, j-1, coordinates_visited)
+            if not [i-1, j] in coordinates_visited:
+                self.generate_list_of_words(word, i-1, j, coordinates_visited)
+            if not [i-1, j+1] in coordinates_visited:
+                self.generate_list_of_words(word, i-1, j+1, coordinates_visited)
+            if not [i, j-1] in coordinates_visited:
+                self.generate_list_of_words(word, i, j-1, coordinates_visited)
+            if not [i, j+1] in coordinates_visited:
+                self.generate_list_of_words(word, i, j+1, coordinates_visited)
+            if not [i+1, j-1] in coordinates_visited:
+                self.generate_list_of_words(word, i+1, j-1, coordinates_visited)
+            if not [i+1, j] in coordinates_visited:
+                self.generate_list_of_words(word, i+1, j, coordinates_visited)
+            if not [i+1, j+1] in coordinates_visited:
+                self.generate_list_of_words(word, i+1, j+1, coordinates_visited)
 
+    def check_dictionary_for_word(self, word):
+        try:
+            meaning = self.dictionary.meaning(word, disable_errors=True)
+            if meaning:
+                return True
+            return False
+        except IndexError as e:
+            pass
+
+    def build_word(self, word, i, j):
+        if 0 <= i < 4 and 0 <= j < 4:
+            return word + self.board[i][j], False
+        return word, True
--- main.py
+++ main.py
@@ -2,8 +2,15 @@ from board import Board
 
 
 def main():
-    Board().setup_board()
+    board = Board()
+    board.setup_board()
+    print(board.board[0])
+    print(board.board[1])
+    print(board.board[2])
+    print(board.board[3])
 
+    board.generate_list_of_words('', 0, 0, [])
+    print(board.words)
 
 if __name__ == '__main__':
     main()
