dd08a1dd2cdb570fa4ec032d22c4421eb1d0f1b0
dstaraster1969 <71042914+dstaraster1969@users.noreply.github.com>
2023-09-07 07:25:32 -0700

    Improved dictionary lookup efficiency

--- board.py
+++ board.py
@@ -18,8 +18,28 @@ class Board:
         ]
         self.words = []
         self.iterations = 0
+        self.dict = Dictionary()
 
     def setup_board(self):
+        # self.board[0][0] = 'T'
+        # self.board[1][0] = 'E'
+        # self.board[2][0] = 'I'
+        # self.board[3][0] = 'E'
+        #
+        # self.board[0][1] = 'S'
+        # self.board[1][1] = 'A'
+        # self.board[2][1] = 'N'
+        # self.board[3][1] = 'H'
+        #
+        # self.board[0][2] = 'E'
+        # self.board[1][2] = 'I'
+        # self.board[2][2] = 'H'
+        # self.board[3][2] = 'O'
+        #
+        # self.board[0][3] = 'C'
+        # self.board[1][3] = 'W'
+        # self.board[2][3] = 'P'
+        # self.board[3][3] = 'L'
         dice = Dice()
         for i in range(0, 4):
             for j in range(0, 4):
@@ -41,9 +61,8 @@ class Board:
             return
         coordinates_visited.append([i, j])
         word = (word + self.board[i][j]).lower()
-        this_dict = Dictionary()
-        if this_dict.check_for_starts_with(word):
-            if len(word) >= 3 and this_dict.check_for_whole_word(word):
+        if self.dict.check_for_starts_with(word):
+            if len(word) >= 3 and self.dict.check_for_whole_word(word):
                 self.words.append(word)
                 # don't return here because there could be additional words that start w/ this word
             await self.check_word(word, deepcopy(coordinates_visited), i, j - 1)
--- dictionary.py
+++ dictionary.py
@@ -1,9 +1,14 @@
+import time
+
 import english_words
 
 
 class Dictionary:
     def __init__(self):
-        self.dictionary = english_words.get_english_words_set(['web2'], lower=True)
+        start = time.time()
+        self.dictionary = sorted(english_words.get_english_words_set(['web2'], lower=True))
+        end = time.time()
+        print(f'time to sort: {end - start}')
 
     def check_for_whole_word(self, word):
         return word in self.dictionary
@@ -12,4 +17,6 @@ class Dictionary:
         for w in self.dictionary:
             if w.startswith(word):
                 return True
+            if word < w:
+                return False
         return False
